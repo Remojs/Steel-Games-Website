29172201656f58606f9f6ebc02c86fe4bcb81f1a
thiago <thiagozambonini24@gmail.com>
2023-05-19 12:34:51 -0300

    update de img, again another commit about the img, i really enjoy this so much...

--- client/src/components/Landing-component/Landing.jsx
+++ client/src/components/Landing-component/Landing.jsx
@@ -5,6 +5,14 @@ import './landing.css'
 const Landing = () => {
   return (
     <div className='component-box'>
+
+      <div className='nav-div'>
+        <nav className='nav'> 
+          <button className='nav-btn'> Login Account </button>
+          <button className='nav-btn install'> <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 9l-5 5-5-5M12 12.8V2.5"/></svg> Install Steel </button>
+        </nav>
+      </div>
+
       <div className='landing'>
         <button className='btn'> <NavLink to='/home' className='btn-link'> Go to Steel </NavLink> </button>
       </div>
