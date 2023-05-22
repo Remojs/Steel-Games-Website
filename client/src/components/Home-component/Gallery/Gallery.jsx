8a25f37592a87f33cdf652eb282da9d579c0a394
thiago <thiagozambonini24@gmail.com>
2023-05-21 23:32:45 -0300

    GIF UPLOAD

--- client/src/components/Home-component/Gallery/Gallery.jsx
+++ client/src/components/Home-component/Gallery/Gallery.jsx
@@ -32,28 +32,41 @@ return (
                         <div className="carrousel-box">
                             <img src={element.image} alt="steel" className="image-carrousel"/>
                             <div className="carrousel-info"> 
-                                <h2 className="carrousel-data">{element.name}</h2>
-                                <h2 className="carrousel-data">{element.genres}</h2>
-                                <div className="carrousel-data">
+                                <h2 className="carrousel-data title">{element.name}</h2>
+                                <div className="carrousel-data genres-container">{element.genres.slice(0, 4).map((genre, index) => {
+                                    return (
+                                        <div key={genre} className="genre">{genre}</div>
+                                    )
+                                })}</div>
+
+                                <div className="carrousel-data steam-data">
+                                    <h3 className=" steam-status"> Ya Disponible </h3>
+                                    <div className="steam-sell"> Lo mas vendido </div>
+                                </div>
+
+                                <div className="carrousel-data bottom-elements">
+                                    <h6 className="status"> Free to Play </h6>
+                                    <div className="carrousel-data platforms">
                                         {hasPlatform(element.platform, 'PC') && (
-                                            <img src="https://raw.githubusercontent.com/Remojs/PI-Videogames/3d6cd47276575d86882e5422b356e1ff7eb0c0d4/client/public/img/SVG/windows-174-svgrepo-com.svg" alt="" className="platform-icon" />
+                                            <img src="https://raw.githubusercontent.com/Remojs/PI-Videogames/398205682e06e841e78424f6cde48adf7838e5bd/client/public/img/SVG/windows-174-svgrepo-com.svg" alt="" className="platform-icon" />
                                         )}
                                         {hasPlatform(element.platform, 'Playstation') && (
-                                            <img src="https://raw.githubusercontent.com/Remojs/PI-Videogames/3d6cd47276575d86882e5422b356e1ff7eb0c0d4/client/public/img/SVG/playstation-svgrepo-com.svg" alt="" className="platform-icon" />
+                                            <img src="https://raw.githubusercontent.com/Remojs/PI-Videogames/398205682e06e841e78424f6cde48adf7838e5bd/client/public/img/SVG/playstation-svgrepo-com.svg" alt="" className="platform-icon" />
                                         )}
                                         {hasPlatform(element.platform, 'Xbox') && (
-                                            <img src="https://raw.githubusercontent.com/Remojs/PI-Videogames/3d6cd47276575d86882e5422b356e1ff7eb0c0d4/client/public/img/SVG/xbox-svgrepo-com.svg" alt="" className="platform-icon" />
+                                            <img src="https://raw.githubusercontent.com/Remojs/PI-Videogames/398205682e06e841e78424f6cde48adf7838e5bd/client/public/img/SVG/xbox-svgrepo-com.svg" alt="" className="platform-icon" />
                                         )}
                                         {hasPlatform(element.platform, 'Nintendo Switch') && (
-                                            <img src="https://raw.githubusercontent.com/Remojs/PI-Videogames/3d6cd47276575d86882e5422b356e1ff7eb0c0d4/client/public/img/SVG/nintendo-switch-svgrepo-com.svg" alt="" className="platform-icon" />
+                                            <img src="https://raw.githubusercontent.com/Remojs/PI-Videogames/398205682e06e841e78424f6cde48adf7838e5bd/client/public/img/SVG/nintendo-switch-svgrepo-com.svg" alt="" className="platform-icon" />
                                         )}
                                         {hasPlatform(element.platform, 'Linux') && (
-                                            <img src="https://raw.githubusercontent.com/Remojs/PI-Videogames/3d6cd47276575d86882e5422b356e1ff7eb0c0d4/client/public/img/SVG/linux-svgrepo-com.svg" alt="" className="platform-icon" />
+                                            <img src="https://raw.githubusercontent.com/Remojs/PI-Videogames/398205682e06e841e78424f6cde48adf7838e5bd/client/public/img/SVG/linux-svgrepo-com.svg" alt="" className="platform-icon" />
                                         )}
                                         {hasPlatform(element.platform, 'macOS') && (
-                                            <img src="https://raw.githubusercontent.com/Remojs/PI-Videogames/3d6cd47276575d86882e5422b356e1ff7eb0c0d4/client/public/img/SVG/apple-173-svgrepo-com.svg" alt="" className="platform-icon" />
+                                            <img src="https://raw.githubusercontent.com/Remojs/PI-Videogames/398205682e06e841e78424f6cde48adf7838e5bd/client/public/img/SVG/apple-173-svgrepo-com.svg" alt="" className="platform-icon" />
                                         )}
-                                    </div>
+                                </div>
+                                </div>
                             </div>
                         </div>
                     )}
